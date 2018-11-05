c6e25e74836e84a4f9a9404945974f6ebc1e565d
zhuxinhao <1838370135@qq.com>
2018-11-05 16:01:47 +0800

    koa-session2的测试demo

---
+++ test/koaSession2Test/main.js
@@ -0,0 +1,20 @@
+const Koa = require("koa");
+const session = require("koa-session2");
+const Store = require("./Store.js");
+
+const app = new Koa();
+
+app.use(session({
+    store: new Store()
+}));
+
+app.use(ctx => {
+    let user = ctx.session.user;
+
+    ctx.session.view = "index";
+});
+
+app.use(ctx => {
+    // refresh session if set maxAge
+    ctx.session.refresh()
+})
